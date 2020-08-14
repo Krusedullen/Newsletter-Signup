381a8801b0419d249872bb0ece1a5aec13e16ff6
ThereseSB <thereseskaar@hotmail.com>
2020-08-14 19:16:26 +0200

    populated my HTML-pages and added their responses and routes to app.js.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -12,17 +13,59 @@ app.use(express.static("public")); //dette kallet sier at mappen ved navn public
 
 app.get("/", function(req,res){
   res.sendFile(__dirname + "/signup.html");
-
-})
+});
 
 app.post("/", function(req, res){
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const url = "https://us17.api.mailchimp.com/3.0/lists/f0283b0f02";
+  //dette er dataen som vi vil sende til API-provideren
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
 
-  console.log(firstName + lastName + email);
+  const jsonData = JSON.stringify(data);
+
+  const options = {
+    method: "POST",
+    auth: "therese1:a3b05c6e3be97a85f95af2283be14354-us17"
+  }
+
+  const request = https.request(url, options, function(response){
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data){
+      //console.log(JSON.parse(data));
+    })
+
+  })
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
 })
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
+
+
+//api-key a3b05c6e3be97a85f95af2283be14354-us17
